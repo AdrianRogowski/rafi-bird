8845c08f6c85bd6e618bf963a18e6074bb1872de
Adrian Monahin Rogowski <adrianmrogowski@gmail.com>
2023-04-30 15:29:43 -0400

    fix pipe gravity issue

--- game.js
+++ game.js
@@ -23,7 +23,7 @@ const config = {
     physics: {
       default: "arcade",
       arcade: {
-        gravity: { y: 1000 },
+        gravity: { y: 0 },
         debug: false,
       },
     },
@@ -45,7 +45,8 @@ let score = 0;
 
 function preload() {
   this.load.image("rafi", "assets/rafi_head.png");
-  this.load.image("pipe", "assets/pipe.png");
+  this.load.image("pipeTop", "assets/pipeTop.png");
+  this.load.image("pipeBottom", "assets/pipeBottom.png");
   this.load.audio("shortE", "assets/shortE.wav");
   this.load.audio("longE", "assets/longE.wav");
 }
@@ -55,12 +56,13 @@ function create() {
     rafi.setCollideWorldBounds(true);
     rafi.displayWidth = 37;
     rafi.displayHeight = 48;
+    rafi.setGravityY(1000);
   
     jumpSound = this.sound.add("shortE");
     gameOver = this.sound.add("longE");
   
     pipes = this.physics.add.group();
-    this.time.addEvent({ delay: 1500, callback: addPipes, callbackScope: this, loop: true });
+    this.time.addEvent({ delay: 500, callback: addPipes, callbackScope: this, loop: true });
   
     this.physics.add.collider(rafi, pipes, () => {
       // Play the extended "E" sound when the game ends
@@ -79,6 +81,11 @@ function create() {
     this.input.keyboard.on("keydown-SPACE", () => {
       jump();
     });
+
+    // Add input event listener for "R" key
+    this.input.keyboard.on("keydown-R", () => {
+        this.scene.restart();
+      });
 }  
 
 function jump() {
@@ -90,39 +97,47 @@ function jump() {
 }
 
 function update() {
-  if (rafi.y > this.sys.game.config.height || rafi.y < 0) {
-    gameOver.play();
-    this.scene.restart();
+    if (rafi.y > this.sys.game.config.height || rafi.y < 0) {
+      // Play the extended "E" sound when the game ends
+      gameOver.play();
+  
+      // Display "Game Over" text
+      this.add.text(240, 260, "Game Over", { fontSize: "32px", fill: "#fff" }).setOrigin(0.5);
+  
+      // Restart the game after a delay
+      this.time.addEvent({
+        delay: 2000,
+        callback: () => {
+          this.scene.restart();
+        },
+        callbackScope: this
+      });
+    }
+  
+    pipes.getChildren().forEach((pipe) => {
+      if (pipe.getBounds().right < 0) {
+        pipe.destroy();
+      }
+    });
   }
-}
 
-function addPipes() {
-    const pipeWidth = 169;
-    const pipeHeight = 420;
+  function addPipes() {
+    const pipeWidth = 85;
+    const pipeHeight = 210;
     const gapHeight = 150;
     const pipeX = this.sys.game.config.width;
     const pipeY = (Math.random() * (pipeHeight / 2)) + (pipeHeight / 4);
   
-    const topPipe = this.physics.add.sprite(pipeX, pipeY - pipeHeight - gapHeight, "pipe");
-    const bottomPipe = this.physics.add.sprite(pipeX, pipeY + gapHeight, "pipe");
+    const topPipe = this.physics.add.sprite(pipeX, pipeY - pipeHeight - gapHeight, 'pipeTop');
+    const bottomPipe = this.physics.add.sprite(pipeX, pipeY + gapHeight, 'pipeBottom');
   
     topPipe.setOrigin(0, 1);
     bottomPipe.setOrigin(0, 0);
-
-    // Set the display width and height of the pipes
-    topPipe.displayWidth = pipeWidth;
-    topPipe.displayHeight = pipeHeight;
-    bottomPipe.displayWidth = pipeWidth;
-    bottomPipe.displayHeight = pipeHeight;
   
-    pipes.add(topPipe);
-    pipes.add(bottomPipe);
-  
-    this.physics.add.existing(topPipe);
-    this.physics.add.existing(bottomPipe);
-  
-    topPipe.setVelocityX(-200);
-    bottomPipe.setVelocityX(-200);
+    topPipe.displayWidth = 85;
+    topPipe.displayHeight = 210;
+    bottomPipe.displayWidth = 85;
+    bottomPipe.displayHeight = 210;
   
     topPipe.allowGravity = false;
     bottomPipe.allowGravity = false;
@@ -130,7 +145,14 @@ function addPipes() {
     topPipe.body.immovable = true;
     bottomPipe.body.immovable = true;
   
-    // Remove pipes when they go off-screen
+    pipes.add(topPipe);
+    pipes.add(bottomPipe);
+  
+    topPipe.setVelocityX(-200);
+    bottomPipe.setVelocityX(-200);
+    topPipe.setVelocityY(0); // Ensures that the top pipe doesn't fall down
+    bottomPipe.setVelocityY(0); // Ensures that the bottom pipe doesn't fall down
+  
     this.time.addEvent({
       delay: 5000,
       callback: () => {
@@ -140,6 +162,11 @@ function addPipes() {
       },
       callbackScope: this
     });
-}
+  }    
 
+function updateScore() {
+    score++;
+    scoreText.setText("Score: " + score);
+    console.log("Score updated"); // Add this line for debugging
+  }
   
